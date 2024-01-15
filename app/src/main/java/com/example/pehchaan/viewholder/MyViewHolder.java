bf456244cd683a9ddcfcde42e9581a6ff8235744
Suryansh Prajapati <itssuryanshprajapati@gmail.com>
2024-01-15 22:25:29 +0530

    remove unnecessary code

--- app/src/main/java/com/example/pehchaan/viewholder/MyViewHolder.java
+++ app/src/main/java/com/example/pehchaan/viewholder/MyViewHolder.java
@@ -4,7 +4,6 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.pehchaan.databinding.CustomItemBinding;
-import com.example.pehchaan.model.Model;
 
 public class MyViewHolder extends RecyclerView.ViewHolder {
     public CustomItemBinding binding;
@@ -12,14 +11,6 @@ public class MyViewHolder extends RecyclerView.ViewHolder {
     public MyViewHolder(@NonNull CustomItemBinding itemView) {
         super(itemView.getRoot());
         binding = itemView;
-    }
-
-    public void bind(Model model) {
-        binding.itemtext.setText(model.getTxt());
-        binding.itemcircleimage.setImageResource(model.getImagename());
-        binding.itemphone.setText(model.getNumber());
-        binding.itemcall.setImageResource(model.getCall());
-        binding.itemmessage.setImageResource(model.getMessage());
     }
 
 }
